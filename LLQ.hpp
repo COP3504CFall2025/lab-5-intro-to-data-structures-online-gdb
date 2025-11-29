70cab3babf949508625d377ce9125f1c05b2205a
Joseph Parayil <josephaparayil@gmail.com>
2025-11-29 18:28:54 -0500

    LLS 4 or smth

--- LLQ.hpp
+++ LLQ.hpp
@@ -11,18 +11,29 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLQ();
+    LLQ() {}
 
     // Insertion
-    void enqueue(const T& item) override;
+    void enqueue(const T& item) override {
+        list.addTail(item);
+    }
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override {
+        T result = peek();
+        list.removeHead();
+        return result;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        if (getSize()==0) throw std::runtime_error("Empty");
+        return list.getHead()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list.getCount();
+    }
 
 };
