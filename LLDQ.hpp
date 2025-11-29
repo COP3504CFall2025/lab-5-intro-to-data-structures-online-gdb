bba6383c898975a6cb26051de9646e96af999d56
Joseph Parayil <josephaparayil@gmail.com>
2025-11-29 18:33:23 -0500

    LLDQ

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,43 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() {}
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        list.addHead(item);
+    }
+
+    void pushBack(const T& item) override  {
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        T result = front();
+        list.removeHead();
+        return result;
+    }
+
+    T popBack() override {
+        T result = back();
+        list.removeTail();
+        return result;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if (getSize()==0) throw std::runtime_error("Empty");
+        return list.getHead()->data;
+    }
+
+    const T& back() const override {
+        if (getSize()==0) throw std::runtime_error("Empty");
+        return list.getTail()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {return list.getCount();}
 };
 
 
